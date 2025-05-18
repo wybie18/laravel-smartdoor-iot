ec41ee36c06cca68d1f157b4e54e4a5ee07618a3
Wybie <lyrncofyn@gmail.com>
2025-05-18 13:59:47 +0800

    fix the rfid_tag bug in the Access log resource

--- app/Http/Resources/LogResource.php
+++ app/Http/Resources/LogResource.php
@@ -18,7 +18,7 @@ class LogResource extends JsonResource
             'id'          => $this->id,
             'success'     => $this->success,
             'notes'       => $this->notes,
-            'rfid_tag'    => new CardResource($this->rfid_tag),
+            'rfid_tag'    => new CardResource($this->rfidTag),
             'door'        => new DoorResource($this->door),
             'access_time' => (new Carbon($this->access_time))->format('M d, Y H:i:s'),
             'created_at'  => (new Carbon($this->created_at))->format('M d, Y H:i:s'),
